e43db2f2ad37a64ca8e5dd16ba20eb0df4df6d44
Fei He <hef740@nenu.edu.cn>
2021-01-26 20:18:56 +0800

    fixed bugs and used col names to access data

--- expression_to_image.py
+++ expression_to_image.py
@@ -16,7 +16,7 @@ def scale_to_RGB(channel,truncated_percent):
     channel_new[channel_new > 255] = 255
     return np.uint8(channel_new)
 
-def correlation_between_images(X_RGB, transformed_RGB):
+def RGB_correlation_between_images(X_RGB, transformed_RGB):
     best_pcc = 0
     best_perm = (0,1,2)
     for perm in permutations(range(3), 3):
@@ -25,6 +25,21 @@ def correlation_between_images(X_RGB, transformed_RGB):
             best_pcc = abs(current_pcc)
             best_perm = perm
     return  best_pcc, best_perm
+
+
+def correlation_between_enhanced_images(X_RGB, transformed_RGB):
+    best_pcc = 0
+    best_perm = (0, 1, 2)
+    for perm in permutations(range(3), 3):
+        # reduce 3-dimension to 1 using UMAP again
+        transformer = umap.UMAP(n_neighbors=15, min_dist=0.2, n_components=1)
+        current_pcc, p_value = pearsonr(transformer.fit(transformed_RGB[:, perm]).transform(transformed_RGB[:, perm]).reshape((-1)), X_RGB)
+        if abs(current_pcc) > best_pcc:
+            best_pcc = abs(current_pcc)
+            best_perm = perm
+        del transformer
+    return best_pcc, best_perm
+
 
 def nan_check_in_datframe():
     pass
@@ -39,19 +54,16 @@ umap_neighbor_num (10 in default): the size of the local neighborhood in UMAP
 umap_min_dist (0.2 in default): control how tightly UMAP is allowed to pack points
 
 '''
-def transform_expression_to_RGB(expression_file, pca_conponent_num = 50, umap_neighbor_num = 10, umap_min_dist = 0.2 ):
+def transform_expression_to_RGB(expression_file, original_RGB = True, pca_conponent_num = 50, umap_neighbor_num = 10, umap_min_dist = 0.2 ):
     data = pd.read_csv(expression_file)
 
     X = data.loc[data['in_tissue'] != 0]
 
-    # read RGB columns to see if needing RGB optimization
-    if np.sum(data.loc[:, ['R','G','B']].values) != 0:
-        original_RGB = False
-    else:
-        original_RGB = True
-        # save original image at spot level
-        save_spot_RGB_to_image(X, expression_file)
-
+    # # read RGB columns to see if needing RGB optimization
+    # if np.sum(X.loc[:, ['R','G','B']].values) != 0:
+    #     original_RGB = False
+    # else:
+    #     original_RGB = True
 
     if 'equa' in data.columns:
         #set gene start idex
@@ -61,7 +73,6 @@ def transform_expression_to_RGB(expression_file, pca_conponent_num = 50, umap_ne
     else:
         gene_start_idx = 9
 
-    # save
     values = X.iloc[:, gene_start_idx:].values
 
     best_pcc = 0
@@ -82,17 +93,25 @@ def transform_expression_to_RGB(expression_file, pca_conponent_num = 50, umap_ne
         print(str(e))
         return
     if original_RGB:
+        # save original image at spot level
+        save_spot_RGB_to_image(X, expression_file)
         #do grid search for the percentile parameters
-        for percentile_0 in range(0, 50):
-            for percentile_1 in range(0, 50):
-                for percentile_2 in range(0, 50):
+        for percentile_0 in range(0, 1):
+            for percentile_1 in range(0, 1):
+                for percentile_2 in range(0, 1):
                     # plot_box_for_RGB(X_transformed)
-                    temp_X_transform = np.zeros_like(X_transformed, np.int8)
+                    # optimize pseudo-RGB according to spatial image
+                    temp_X_transform = X_transformed.copy()
                     temp_X_transform[:, 0] = scale_to_RGB(X_transformed[:, 0], percentile_0)
                     temp_X_transform[:, 1] = scale_to_RGB(X_transformed[:, 1], percentile_1)
                     temp_X_transform[:, 2] = scale_to_RGB(X_transformed[:, 2], percentile_2)
+                    if 'equa' in data.columns:
+                        pcc, perm = correlation_between_enhanced_images(X['equa'].values, temp_X_transform)
+                    else:
 
-                    pcc, perm = correlation_between_images(X.iloc[:, 6:9].values, temp_X_transform)
+                        pcc, perm = RGB_correlation_between_images(X[['R','G','B']].values, temp_X_transform)
+
+
                     if best_pcc < pcc:
                         best_pcc = pcc
                         # best_transformed_X = temp_X_transform[:, perm]
@@ -132,8 +151,8 @@ def plot_box_for_RGB(RGB_data):
 def save_transformed_RGB_to_image_and_csv(X, X_transformed, gene_start_idx ,data_file_name):
 
     # full size spot number
-    spot_row = X.loc[:, 'pxl_col_in_fullres'].values
-    spot_col = X.loc[:, 'pxl_row_in_fullres'].values
+    spot_row = X['pxl_col_in_fullres'].values
+    spot_col = X['pxl_row_in_fullres'].values
 
     max_row = np.int(np.max(spot_row + 1))
     max_col = np.int(np.max(spot_col + 1))
@@ -156,12 +175,12 @@ def save_transformed_RGB_to_image_and_csv(X, X_transformed, gene_start_idx ,data
     cv2.imwrite(os.path.splitext(data_file_name)[0] + '_transformed.jpg', img)
 
     # save transformed_X back to csv file
-    X.insert(gene_start_idx, 'transformed_R',X_transformed[:, 0], True)
-    X.insert(gene_start_idx+1, 'transformed_G',X_transformed[:, 1], True)
-    X.insert(gene_start_idx+2, 'transformed_B',X_transformed[:, 2], True)
-
-    # #then save to csv file
-    X.iloc[:, 0:gene_start_idx+3].to_csv(os.path.splitext(data_file_name)[0] + '_newRGB.csv', index = False)
+    # X.insert(gene_start_idx, 'transformed_R',X_transformed[:, 0], True)
+    # X.insert(gene_start_idx+1, 'transformed_G',X_transformed[:, 1], True)
+    # X.insert(gene_start_idx+2, 'transformed_B',X_transformed[:, 2], True)
+    #
+    # # #then save to csv file
+    # X.iloc[:, 0:gene_start_idx+3].to_csv(os.path.splitext(data_file_name)[0] + '_newRGB.csv', index = False)
 
     del img, spot_row, spot_col#, mask, inpaint_img,  resize_contour#resize_img,
 
@@ -176,15 +195,14 @@ def save_pseudoRGB_to_spot_level(X, gene_start_idx, data_path):
         cv2.circle(img, (spot_col[index], spot_row[index]), radius= 68,
                    color=(int(values[index][0]), int(values[index][1]), int(values[index][2])),
                    thickness=-1)
-    resize_img = cv2.resize(img, None, fx=0.25, fy=0.25, interpolation=cv2.INTER_CUBIC)
-    cv2.imwrite(os.path.splitext(data_path)[0] + '_spot_rect.jpg', resize_img)
+    cv2.imwrite(os.path.splitext(data_path)[0] + '_spot_circle.jpg', img)
     del values, spot_row, max_col, img
 
 def save_spot_RGB_to_image(X, data_file ):
 
     values = X[['R','G','B']].values
-    spot_row = X.iloc[:, 'pxl_col_in_fullres'].values
-    spot_col = X.iloc[:, 'pxl_row_in_fullres'].values
+    spot_row = X['array_row'].values
+    spot_col = X['array_col'].values
 
     max_row = np.int(np.max(spot_row))
     max_col = np.int(np.max(spot_col))
@@ -200,9 +218,9 @@ def save_spot_RGB_to_image(X, data_file ):
 
 def save_enhanced_RGB_to_image(X, data_file):
 
-    values = X[:,'equa'].values
-    spot_row = X.iloc[:, 'pxl_col_in_fullres'].values
-    spot_col = X.iloc[:, 'pxl_row_in_fullres'].values
+    values = X['equa'].values
+    spot_row = X['array_row'].values
+    spot_col = X['array_col'].values
 
     max_row = np.int(np.max(spot_row))
     max_col = np.int(np.max(spot_col))
@@ -214,14 +232,14 @@ def save_enhanced_RGB_to_image(X, data_file):
 
     resize_img = cv2.resize(img, None, fx=5, fy=5, interpolation=cv2.INTER_CUBIC)
     cv2.imwrite(os.path.splitext(data_file)[0] + '_enhanced.jpg', resize_img)
-    del values, spot_row, max_col, img, resize_img
+    del spot_row, max_col, img, resize_img
 
+if __name__ == '__main__':
+    #
+    # data_folder = r'/home/fei/Desktop/test/Brain/'
+    # for expression_file in os.listdir(data_folder):
+    #     if os.path.splitext(expression_file)[1] == '.csv':
+    #         transform_expression_to_RGB(os.path.join(data_folder, expression_file))
 
-#
-data_folder = r'/home/fei/Desktop/test/Brain/'
-for expression_file in os.listdir(data_folder):
-    if os.path.splitext(expression_file)[1] == '.csv':
-        transform_expression_to_RGB(os.path.join(data_folder, expression_file))
-
-
-
+    data_file_name = r'/home/fei/Desktop/single_cell_spatial_image-main/vel_data.csv'
+    transform_expression_to_RGB(data_file_name, True, pca_conponent_num=10)
