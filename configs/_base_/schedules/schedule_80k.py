6f3b5cd0f3e28a16524b7ddf766a769baa6ae937
lijingyi <39088650+lijingyimm@users.noreply.github.com>
2021-08-06 15:25:15 +0800

    add QA in training pipeline

--- configs/_base_/schedules/schedule_80k.py
+++ configs/_base_/schedules/schedule_80k.py
@@ -5,5 +5,5 @@ optimizer_config = dict()
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=True)
 # runtime settings
 runner = dict(type='EpochBasedRunner', max_epochs=50)
-checkpoint_config = dict(by_epoch=True, interval=50)
+checkpoint_config = dict(by_epoch=True, interval=1)
 evaluation = dict(interval=1, metric='mIoU')
